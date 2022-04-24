accfa2beb916dcb4341ed26432199166ba9d4244
Qadeem Qureshi <alexgrantnrp@gmail.com>
2022-04-24 16:55:11 -0500

    Sounds have been added!

---
+++ John The Zombie Hunter-Unity/Assets/Scripts/EnemyAI_SoundController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI_SoundController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource[] attackSounds;
+    public AudioSource[] searchSounds;
+    public AudioSource damagedSound;
+    public float lastSoundTime;
+    public void ApplySearchSound()
+    {
+        if (lastSoundTime > Time.time) return;
+        searchSounds[Random.Range(0, searchSounds.Length)].Play();
+        lastSoundTime = Time.time + Random.Range(4, 10);
+    }
+    public void ApplyAttackSound()
+    {
+        attackSounds[Random.Range(0, attackSounds.Length)].Play();
+    }
+    public void ApplyDamageSound()
+    {
+        damagedSound.Play();
+    }
+}
